649208d4b7e3a64e1c6d5aa24d20cb81258dd085
Jeremy Petersen <godofkratos3@protonmail.com>
2026-04-15 14:38:42 +0800

    Complete the Rock Paper Scissors Game

--- script.js
+++ script.js
@@ -12,9 +12,64 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanChoice = window.prompt("What is your choice?")
+    let input = window.prompt("What is your choice?")
+    let humanChoice = input.toLowerCase();
     return humanChoice;
 }
 
-console.log("Computer Choice: " + getComputerChoice());
-console.log("Human Choice: " + getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock") {
+        if (computerChoice == "paper") {
+            computerScore++;
+            return "Computer wins!"
+        } else if (computerChoice == "scissors") {
+            humanScore++;
+            return "Human wins!"
+        } else if (computerChoice == "rock") {
+            return "Tie!"
+        }
+    } else if (humanChoice == "paper") {
+        if (computerChoice == "paper") {
+            return "Tie!"
+        } else if (computerChoice == "rock") {
+            humanScore++;
+            return "Human wins!"
+        } else if (computerChoice == "scissors") {
+            computerScore++;
+            return "Computer wins!"
+        }
+    } else if (humanChoice == "scissors") {
+        if (computerChoice == "scissors"){
+            return "Tie!"
+        } else if (computerChoice == "paper") {
+            humanScore++;
+            return "Human wins!"
+        } else if (computerChoice == "rock") {
+            computerScore++;
+            return "Computer Wins!"
+        }
+    }
+}
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        let ComputerSelection = getComputerChoice();
+        let HumanSelection = getHumanChoice();
+        console.log("Computer Chose: " + ComputerSelection);
+        console.log("Human Chose: " + HumanSelection);
+        console.log(playRound(HumanSelection, ComputerSelection));
+        console.log("Computer Score: " + computerScore + " | " + "Human Score: " + humanScore);
+    }
+}
+
+let computerScore = 0;
+let humanScore = 0;
+
+
+
+playGame();
+
+
+
+
+
