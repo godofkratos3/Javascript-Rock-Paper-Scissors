cad26e8b9f2c376d231425e8717cd9fbba351932
Jeremy Petersen <godofkratos3@protonmail.com>
2026-04-15 13:53:54 +0800

    Add getComputerChoice() and getHumanChoice()

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+
+function getComputerChoice() {
+    let computerChoice = Math.random();
+
+    if (computerChoice < .33) {
+        return "rock"
+    } else if (computerChoice < .66) {
+        return "paper"
+    } else if (computerChoice < 1) {
+        return "scissors"
+    }
+}
+
+function getHumanChoice() {
+    let humanChoice = window.prompt("What is your choice?")
+    return humanChoice;
+}
+
+console.log("Computer Choice: " + getComputerChoice());
+console.log("Human Choice: " + getHumanChoice());
